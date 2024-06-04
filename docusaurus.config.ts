c80f60325629fce18a2de310f6b89800ec757860
shoma46 <shoma.sakaue@lisse-law.co.jp>
2024-06-04 14:37:18 +0900

    🔧 fix: 設定ファイルを更新

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://shoma4646.github.io/doc/",
+  url: "https://shoma4646.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/doc/",
