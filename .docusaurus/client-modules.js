70f3b65ce266bddb8aa820639072dab147b0b604
shoma46 <shoma.sakaue@lisse-law.co.jp>
2024-06-04 16:07:02 +0900

    🎨 fix: 不要なファイル削除

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/shoma/Develop/MyProjects/plantuml/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/shoma/Develop/MyProjects/plantuml/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/shoma/Develop/MyProjects/plantuml/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/shoma/Develop/MyProjects/plantuml/src/css/custom.css"),
-];
